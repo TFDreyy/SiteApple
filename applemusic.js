314d2379ade8e8c3311f8f1f1092ae6453b8f26f
Lucas Fonseca <fonsecafluc@gmail.com>
2025-09-13 00:33:27 -0300

    15 commit de Apple Music, agora com todo o conteúdo da página (sujeito a ajustes finais

--- applemusic.js
+++ applemusic.js
@@ -3,3 +3,12 @@ document.querySelectorAll('.card').forEach(card => {
   card.addEventListener('mouseenter', () => video.play());
   card.addEventListener('mouseleave', () => video.pause());
 });
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const question = item.querySelector(".faq-question");
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
